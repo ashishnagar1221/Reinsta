7ce17674aa6f7ef20da7815bbe4432d7a608338f
Ashish <ashishnagar1221@gmail.com>
2020-04-25 19:55:14 +0530

    Home UI done

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Home = () => {
   return(
-    <h1>Home</h1>
+    <div className="home">
+      <div className="card home-card">
+      <h5>Cristopher Nolan</h5>
+        <div className="card-image"></div>
+          <img src="https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+          <div className="card-content">
+          <i class="material-icons" style={{color:"red"}} >favorite</i>
+        <h6>title</h6>
+        <p>this is amazing post</p>
+        <input type="text" placeholder ="add a comment"/>
+      </div>
+
+      <h5>Cristopher Nolan</h5>
+        <div className="card-image"></div>
+          <img src="https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+          <div className="card-content">
+          <i class="material-icons" style={{color:"red"}} >favorite</i>
+        <h6>title</h6>
+        <p>this is amazing post</p>
+        <input type="text" placeholder ="add a comment"/>
+      </div>
+
+      <h5>Cristopher Nolan</h5>
+        <div className="card-image"></div>
+          <img src="https://images.unsplash.com/photo-1439694458393-78ecf14da7f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+          <div className="card-content">
+          <i class="material-icons" style={{color:"red"}}>favorite</i>
+        <h6>title</h6>
+        <p>this is amazing post</p>
+        <input type="text" placeholder ="add a comment"/>
+      </div>
+      </div>
+    </div>
    )
 
  }
